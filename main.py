60d5879c706cd2977e9ef783369083af6b433690
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-21 20:26:42 +0100

    Defined the def's

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ __winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
 __human_name__ = 'functions'
 
 # Add your code after this line
+
+def greet(name):
+   return print (f"Hello, {name}!")
+greet("Bob")
+
+def add( a, b, c):
+    return print (a + b + c)
+add(5,3,2)
+
+def positive(num):
+    return  print ( num > 0)
+positive(1)
+
+def negative(num):
+    return print ( num > 0 )
+negative(-2)
