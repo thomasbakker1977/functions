4b7b9e8278815322165f5c13d1ffdf9c214886ca
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-23 17:29:46 +0100

    Replaced the print statement outside of the function

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ __human_name__ = 'functions'
 # Add your code after this line
 
 def greet(name):
-   return print (f"Hello, {name}!")
-greet("Bob")
+   return  (f"Hello, {name}!")
+print(greet("Bob"))
 
 def add( a, b, c):
-    return print (a + b + c)
-add(5,3,2)
+    return  (a + b + c)
+print (add(5,3,2))
 
 def positive(num):
-    return  print ( num > 0)
-positive(5)
+    return   ( num > 0)
+print(positive(5))
 
 def negative(num):
-    return print ( num < 0 )
-negative(5)
+    return  ( num < 0 )
+print(negative(5))
